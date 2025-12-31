38ce1b845580bd129f1a2173f07bac8748be645d
fauzanhaziz <fauzan17haziz@gmail.com>
2025-12-31 11:58:54 +0700

    menghapus prop pathname yang menyebabkan error build

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -87,7 +87,6 @@ export default function AdminLayout({ children }: { children: React.ReactNode })
         className={`flex-1 flex flex-col min-w-0 transition-all duration-300 ease-in-out ${mainContentMargin}`}
       >
         <AdminHeader
-          pathname={pathname}
           isSidebarCollapsed={isDesktopCollapsed}
           onToggleSidebar={() => {
             if (!isDesktop) {
